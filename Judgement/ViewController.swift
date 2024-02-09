fbd76135680b6a4c37e129472511f5a154be5b67
Manan Goyal(manan.goyal) <manan.goyal@lalamove.com>
2024-02-09 14:33:26 +0800

    Initial Commit

---
+++ Judgement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Judgement
+//
+//  Created by manan.goyal on 9/2/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
